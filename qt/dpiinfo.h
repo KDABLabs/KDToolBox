b047b69fe72e69c9a695126d9905cda01fee1cdb
Sergio Martins <sergio.martins@kdab.com>
2021-02-12 11:48:13 +0000

    Add function that prints HDPI related info
    Prints env variables, screen properties, default font and such
    Probably too small to link in README
    
    Change-Id: If8cb653a188fff11cb89ec4a3d0114a8018a7dd6
    Reviewed-on: https://codereview.kdab.com/c/kdab/KDToolBox/+/92698
    Reviewed-by: Jan Arne Petersen <jan.petersen@kdab.com>

---
+++ qt/dpiinfo.h
@@ -0,0 +1,80 @@
+/****************************************************************************
+**                                MIT License
+**
+** Copyright (C) 2020 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+** Author: Sérgio Martins <sergio.martins@kdab.com>
+**
+** This file is part of KDToolBox (https://github.com/KDAB/KDToolBox).
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, ** and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions:
+**
+** The above copyright notice and this permission notice (including the next paragraph)
+** shall be included in all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF ** CONTRACT, TORT OR OTHERWISE,
+** ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+** DEALINGS IN THE SOFTWARE.
+****************************************************************************/
+
+#ifndef KDTOOLBOX_DPIINFO_H
+#define KDTOOLBOX_DPIINFO_H
+
+#include <QFont>
+#include <QGuiApplication>
+#include <QScreen>
+#include <QString>
+#include <QStringList>
+
+namespace KDToolBox {
+
+inline QString hdpiInfo()
+{
+    const auto envVars = { "QT_AUTO_SCREEN_SCALE_FACTOR",
+                           "QT_STYLE_OVERRIDE",
+                           "QT_SCREEN_SCALE_FACTORS",
+                           "QT_DEVICE_PIXEL_RATIO",
+                           "QT_ENABLE_HIGHDPI_SCALING",
+                           "QT_SCALE_FACTOR",
+                           "QT_FONT_DPI",
+                           "QT_USE_PHYSICAL_DPI" };
+
+    const QFont font = qApp->font();
+    QString result = QStringLiteral("defaultFontPx=%1; defaultFontPt=%2; app.dpr=%3; qpa=%4\n")
+                         .arg(font.pixelSize())
+                         .arg(font.pointSize())
+                         .arg(qApp->devicePixelRatio())
+                         .arg(qApp->platformName());
+    for (const char *envVar : envVars) {
+        if (qEnvironmentVariableIsSet(envVar)) {
+            result += QStringLiteral("%1=%2; ")
+                          .arg(QString::fromLatin1(envVar))
+                          .arg(qEnvironmentVariable(envVar));
+        }
+    }
+
+    const QList<QScreen *> screens = qApp->screens();
+    for (QScreen *screen : screens) {
+        result += QStringLiteral("\nScreen %1: %2x%3, physDpi=%4, logicalDpi=%5, dpr=%6")
+                      .arg(screen->name())
+                      .arg(screen->size().width())
+                      .arg(screen->size().height())
+                      .arg(screen->physicalDotsPerInch())
+                      .arg(screen->logicalDotsPerInch())
+                      .arg(screen->devicePixelRatio());
+    }
+
+    return result;
+}
+
+}
+
+#endif
